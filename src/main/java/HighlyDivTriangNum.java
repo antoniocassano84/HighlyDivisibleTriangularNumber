43e676c9e3293138b2486ec57f3db17d73406582
antoniocassano84 <antonio.cassano84@gmail.com>
2019-12-02 20:42:40 +0000

    change long to int

--- src/main/java/HighlyDivTriangNum.java
+++ src/main/java/HighlyDivTriangNum.java
@@ -4,20 +4,20 @@ import java.util.List;
 class HighlyDivTriangNum {
 
     static void printDivisors(int maxNum) {
-        long n = 0L, m = 1L;
-        List<Long> divs = new ArrayList<>();
+        int n = 0, m = 1;
+        List<Integer> divs = new ArrayList<>();
         while(divs.size() <= maxNum) {
             n += m++;
             divs.clear();
             System.out.print(n + ": ");
-            for (long i = 1L; i <= n; i++) if (n % i == 0) divs.add(i);
-            for (long l : divs) System.out.print(l + " ");
-            System.out.println();
+            for (int i = 1; i <= n; i++) if (n % i == 0) divs.add(i);
+            for (int i : divs) System.out.print(i + " ");
+            System.out.println("- #" + divs.size());
         }
     }
 
 
     public static void main(String args[]) {
-        printDivisors(500);
+        printDivisors(5);
     }
 }
